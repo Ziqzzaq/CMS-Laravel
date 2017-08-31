bfbcd9aa9581428185b5c37be2b233d182fbb5fc
Michał Borzęcki <ziqzzaq.web@gmail.com>
2017-08-31 12:03:11 +0200

    Poprawki widoku artykułów

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -16,13 +16,14 @@
                         <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/Tc_TO5dAUDE" frameborder="0" allowfullscreen></iframe>
                     </div>
                     <div class="card-content">
-                        <a href="single_video.html">
+
                             <h4>{{$article->title}}</h4>
-                        </a>
+
                         <p>{{$article->contents}}</p>
                         <span class="upper-label">Dodał</span>
                         <span class="article-author">Michał Borzęcki</span>
                         <span class="article-date">Utworzony: {{$article->created_at}}</span>
+                        <a class="read-more" href="single-article">Czytaj więcej...</a>
                     </div>
                 </div>
             </div>
