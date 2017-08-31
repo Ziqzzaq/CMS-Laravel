8b06deaf5cf8a261af9c287bea9112b276165ade
Michał Borzęcki <ziqzzaq.web@gmail.com>
2017-08-31 14:17:53 +0200

    Wstępna struktura formularza

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -1,1 +1,25 @@
-<h2>Nowy artykuł!</h2>
+@extends('master')
+@section('content')
+
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="card">
+                <div class="panel-body">
+                    <!-- Formularz -->
+
+                    {!! Form::open(['url'=>'articles','class'=>'form-horizontal']) !!}
+
+                        <div class="form-group">
+                            <label for="" class="col-md-4 control-label">Tytuł</label>
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" />
+                            </div>
+                        </div>
+
+                    {!! Form::close() !!}
+                </div>
+            </div>
+        </div>
+    </div>
+
+@stop
