d438773fc02481cae38e5c1d8f96394ed9ff9cfb
Michał Borzęcki <ziqzzaq.web@gmail.com>
2017-08-31 16:50:16 +0200

    Optymalizacja części wspólnych formularzy

--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -7,41 +7,11 @@
                 <div class="panel-body">
                     <!-- Formularz -->
 
-                    @if(count($errors) > 0)
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
+                    @include('articles.form_errors')
 
                     {!! Form::model($article, ['method'=>'PATCH', 'class'=>'form-horizontal', 'action'=>['ArticlesController@update', $article->id]]) !!}
 
-                    <div class="form-group">
-                        <div class="col-md-4 control-label">
-                            {!! Form::label('title','Tytuł') !!}
-                        </div>
-                        <div class="col-md-6">
-                            {!! Form::text('title',null,['class'=>'form-control']) !!}
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <div class="col-md-4 control-label">
-                            {!! Form::label('contents','Treść') !!}
-                        </div>
-                        <div class="col-md-6">
-                            {!! Form::textarea('contents',null,['class'=>'form-control']) !!}
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <div class="col-md-6 col-md-offset-4">
-                            {!! Form::submit('Dodaj artykuł',['class'=>'btn btn-primary']) !!}
-                        </div>
-                    </div>
+                        @include('articles.form')
 
                     {!! Form::close() !!}
                 </div>
